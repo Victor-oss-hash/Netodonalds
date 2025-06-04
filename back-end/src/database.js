0db90a73d618b4eb3c1d65f9f6e2ed0519a9e114
victor <flugelddlc@gmail.com>
2025-06-04 17:01:15 -0300

    Delete back-end/src directory

--- back-end/src/database.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-
-const URL_PG = "postgresql://postgres:123@localhost:5432/NetoLanches";
-
-const database = new Pool({
-  connectionString: URL_PG,
-});
-
-module.exports = database;
